8aaac1a336c163765ae0b4fb3da94699d0da4ab2
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-07-17 16:38:07 -0400

    Create models: Position, TaskType, Worker, Task

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True,)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True,)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255, unique=True,)
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
