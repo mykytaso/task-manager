01af37078e62044c5b2522eaaf90cbaa1af49ba4
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-07-18 10:25:12 -0400

    Add __str__ to all models, add new fields to Task model + migrations

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,9 +7,15 @@ from django.db import models
 class Position(models.Model):
     name = models.CharField(max_length=255, unique=True,)
 
+    def __str__(self: Position) -> str:
+        return self.name
+
 
 class TaskType(models.Model):
     name = models.CharField(max_length=255, unique=True,)
+
+    def __str__(self: TaskType) -> str:
+        return self.name
 
 
 class Worker(AbstractUser):
@@ -17,11 +25,34 @@ class Worker(AbstractUser):
         null=True,
     )
 
+    def __str__(self: Worker) -> str:
+        return f'{self.username} – {self.position}'
+
 
 class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ('Critical', 'Critical'),
+        ('High', 'High'),
+        ('Medium', 'Medium'),
+        ('Low', 'Low'),
+        ('Optional', 'Optional'),
+    )
+
     name = models.CharField(max_length=255, unique=True,)
+    description = models.TextField(blank=True, null=True,)
+    deadline = models.DateField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False,)
+    priority = models.CharField(
+        max_length=255,
+        choices=PRIORITY_CHOICES,
+    )
     task_type = models.ForeignKey(
         TaskType,
         on_delete=models.SET_NULL,
         null=True,
     )
+    assignees = models.ManyToManyField(Worker, related_name='tasks',)
+
+    def __str__(self: Task) -> str:
+        return (f'{self.name} (Type: {self.task_type}, '
+                f'Priority: {self.priority}, Deadline: {self.deadline})')
