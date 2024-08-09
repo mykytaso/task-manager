2cfdbf54288b70d27d389c9964e9dcb493cf6abc
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-08-09 14:57:15 -0400

    Delete static folder and move styles to base.html head

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -63,8 +63,7 @@ ROOT_URLCONF = "it_company_task_manager.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -129,10 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
