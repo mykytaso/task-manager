1c667762bc0944a8bc38d87175e005abecbb8c7a
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-08-09 15:28:19 -0400

    Add allowed hosts to settings.py

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -28,8 +28,13 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG_VALUE") == "True"
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost"
+]
 
 
 # Application definition
